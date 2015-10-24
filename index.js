aa023f826e4e1fd7b204422c1a4f8ff4a7fadf72
Isaiah Simpson <isaiah.simpson@gmail.com>
2015-10-24 14:02:58 -0500

    rename "static" url root to "project", migrate from minimist to commander

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 
+/* globals require, process, __dirname */
+
 'use strict';
 
 let _ = require('lodash'),
@@ -7,28 +9,38 @@ let _ = require('lodash'),
     path = require('path'),
     express = require('express'),
     http = require('http'),
-    argv = require('minimist')(process.argv.slice(2)),
-    serveIndex = require('serve-index'),
-    port = argv.p || argv.port || '5678',
-    staticDir = path.resolve(process.cwd(), argv._ && argv._.length > 0 ? argv._[0] : ''),
-    go = {cwd: staticDir},
-    projectName = staticDir.split(path.sep).pop(),
+    program = require('commander'),
+    serveIndex = require('serve-index');
+
+program
+    .version('0.0.0')
+    .usage('[options] <file ...>')
+    .option('-p, --port <portNumber>', 'serve spec runners on the specified local port number', parseInt, 5678)
+    .option('-r, --run [specName]', 'run the specified spec headless')
+    .parse(process.argv);
+
+let projectDir = path.resolve(process.cwd(), program.args && program.args.length > 0 ? program.args[0] : ''),
+    go = {cwd: projectDir},
+    projectName = projectDir.split(path.sep).pop(),
     jasmineDir = `${__dirname}/node_modules/jasmine-core`,
     jasminumDir = `${__dirname}/web`;
 
+//
+//function headless() {
+//
+//}
+//
 function start(systemJs, configJs) {
     let app = express();
 
     app.set('view engine', 'jade');
     app.set('views', `${__dirname}/views`);
-    app.use('/static', serveIndex(staticDir, {icons: true}));
-    app.use('/static', express.static(staticDir));
+    app.use('/project', serveIndex(projectDir, {icons: true}));
+    app.use('/project', express.static(projectDir));
     app.use('/jasmine', serveIndex(jasmineDir, {icons: true}));
     app.use('/jasmine', express.static(jasmineDir));
     app.use('/jasminum', serveIndex(jasminumDir, {icons: true}));
     app.use('/jasminum', express.static(jasminumDir));
-
-    //console.log(`/static => ${staticDir}`);
 
     app.get('/', (req, res) => {
         glob('!(node_modules|jspm_packages)/**/*-spec.js', go, (err, files) => {
@@ -52,7 +64,7 @@ function start(systemJs, configJs) {
             } else {
                 res.status(200).render('spec', {
                     specName: req.params.specName,
-                    specJs: `/static/${files[0]}`,
+                    specJs: `/project/${files[0]}`,
                     systemJs,
                     configJs
                 });
@@ -66,14 +78,14 @@ function start(systemJs, configJs) {
                 projectName,
                 systemJs,
                 configJs,
-                specsJs: _.map(files, file => `/static/${file}`).join('\', \'')
+                specsJs: _.map(files, file => `/project/${file}`).join('\', \'')
             });
         });
     });
 
     http
         .createServer(app)
-        .listen(port, () => console.log(`HTTP server listening on port ${port}`));
+        .listen(program.port, () => console.log(`HTTP server listening on port ${program.port}`));
 }
 
 glob(`jspm_packages/system.js`, go, (error, files) => {
@@ -82,13 +94,13 @@ glob(`jspm_packages/system.js`, go, (error, files) => {
     if (error || !files || files.length === 0) {
         console.error('Cannot start Jasminum - Unable to locate system.js for JSPM.');
     } else {
-        systemJs = `/static/${files[0]}`;
+        systemJs = `/project/${files[0]}`;
 
         glob(`config.js`, go, (error, files) => {
             if (error || !files || files.length === 0) {
                 console.error('Cannot start Jasminum - SystemJS config.js file not found.');
             } else {
-                configJs = `/static/${files[0]}`;
+                configJs = `/project/${files[0]}`;
                 start(systemJs, configJs);
             }
         });
