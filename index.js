5b2b0b0d164b3383236629c3b98f0ca5cc05de44
bwain <bwain@grubhub.com>
2017-03-11 14:23:50 -0600

    readme and change default port

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ program
     .usage('[options] <file ...>')
     .option('--webpackConfig <webpackConfig>', 'webpackConfigLocation')
     .option('--port <portNumber>', 'serve spec list on the specified local port number', 5678)
-    .option('--webpackPort <webpackPort>', 'local webpack dev server port ', 8080)
+    .option('--webpackPort <webpackPort>', 'local webpack dev server port ', 5679)
     .parse(process.argv);
 
 if(!program.webpackConfig){
